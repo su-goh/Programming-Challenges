88f9613b37e208ac297f6087fc1a207c9467b806
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2020-11-24 17:29:18 -0600

    Update largest-number.java

--- largest-number.java
+++ largest-number.java
@@ -1,30 +1,21 @@
 class Solution {
-    public String largestNumber(int[] nums) {
-        
-        String[] strNums = new String[nums.length];
-        for(int i = 0; i < nums.length; i++) {
-            strNums[i] = nums[i] + "";
-        }
-        
-        Comparator<String> comp = new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                String str1 = s1 + s2;
-                String str2 = s2 + s1;
-                return str2.compareTo(str1);
-            }
-        };
-        
-        Arrays.sort(strNums, comp);
-        if(strNums[0].equals("0")) return "0";
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for(String num: strNums) {
-            // System.out.println(num);
-            sb.append(num);
-        }
-               
-        return sb.toString();
-    }
+    public String largestNumber(int[] nums) {
+        
+        String[] strNums = new String[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            strNums[i] = String.valueOf(nums[i]);
+        }
+        
+        
+        Arrays.sort(strNums, (s1, s2) -> (s2+s1).compareTo(s1+s2));
+        if(strNums[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < strNums.length; i++) {
+            sb.append(strNums[i]);
+        }
+               
+        return sb.toString();
+    }
 }
