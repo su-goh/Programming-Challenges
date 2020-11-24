d6052dbaf47bb705307059d2a0ff6c96ab800aa3
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2020-11-23 23:19:13 -0600

    Update partition-equal-subset-sum.java

--- partition-equal-subset-sum.java
+++ partition-equal-subset-sum.java
@@ -1,38 +1,35 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        
-        int sum = 0;
-        
-        for(int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-        }
-        
-        if(sum % 2 != 0) return false;
-        else sum /= 2;
-        
-        boolean[][] dp = new boolean[nums.length + 1][sum+1];
-        // for(int i = 0; i < sum; i++) {
-        //     dp[nums.length][i] = true;
-        // }
-        
-        for(int i = 0; i <= nums.length; i++) {
-            dp[i][0] = true;
-        }
-        
-        
-        for(int i = nums.length - 1; i >= 0; i--) {
-            for(int j = 0; j <= sum; j++) {
-                
-                // if(nums[i] > j) {
-                dp[i][j] = dp[i+1][j];
-                if(nums[i] <= j) {
-                    dp[i][j] = dp[i][j] || dp[i+1][j-nums[i]];
-                    // System.out.println("hey " + j + " " + (nums[i]));
-                }
-                // System.out.println(i + " " + j + ": " + dp[i][j]);
-            }
-        }
-        
-        return dp[0][sum];
-    }
+    public boolean canPartition(int[] nums) {
+        
+        int sum = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        
+        if(sum % 2 != 0) return false;
+        else sum /= 2;
+        
+        boolean[][] dp = new boolean[nums.length + 1][sum+1];
+
+        
+        for(int i = 0; i <= nums.length; i++) {
+            dp[i][0] = true;
+        }
+        
+        
+        for(int i = nums.length - 1; i >= 0; i--) {
+            for(int j = 1; j <= sum; j++) {
+                
+                if(nums[i] > j) {
+                    dp[i][j] = dp[i+1][j];
+                }
+                else {
+                    dp[i][j] = dp[i+1][j] || dp[i+1][j-nums[i]];
+                }
+            }
+        }
+        
+        return dp[0][sum];
+    }
 }
