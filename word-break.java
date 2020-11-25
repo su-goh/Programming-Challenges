764d42a9ef384c5ac13aaac4891fdf4975bd177b
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2020-11-25 01:03:44 -0600

    working commit - Created using LeetHub

---
+++ word-break.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        boolean[] dp = new boolean[s.length()];
+        
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 0; j <= i; j++) {
+                String subStr = s.substring(j, i+1);
+                
+                if(wordDict.contains(subStr) && (j==0 || dp[j-1])) {
+                    dp[i] = true;
+                }
+            }
+        }
+        
+        return dp[s.length() - 1];
+    }
+}
