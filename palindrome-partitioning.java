a559a35173b563033b63a086feded6b933fca809
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2020-11-25 23:46:26 -0600

    Update palindrome-partitioning.java

--- palindrome-partitioning.java
+++ palindrome-partitioning.java
@@ -1,45 +1,46 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        if(s.length() == 0 || s == null) return new ArrayList<>();
-        
-        List<List<String>> ret = new ArrayList<>();
-        
-        helper(s, ret, new ArrayList<>());
-        
-        return ret;
-    }
-    
-    public void helper(String s, List<List<String>> ret, List<String> currList) {
-        // base case
-        if(s.length() == 0) {
-            ret.add(new ArrayList<>(currList));
-            return;
-        }
-        
-        for(int i = 0; i < s.length(); i++) {
-            String temp = s.substring(0,i+1);
-            if(!isPalindrone(temp)) continue;
-            
-            currList.add(temp);
-            helper(s.substring(i+1,s.length()), ret, currList);
-            currList.remove(currList.size() - 1);
-        }
-        
-        return;
-    }
-    
-    public boolean isPalindrone(String s) {
-        int p1 = 0, p2 = s.length() - 1;
-        while(p1 < p2) {
-            if(s.charAt(p1) == s.charAt(p2)) {
-                p1++;
-                p2--;
-                continue;
-            } else {
-                return false;
-            }
-        }
-        
-        return true;
-    }
+    public List<List<String>> partition(String s) {
+        if(s.length() == 0 || s == null) return new ArrayList<>();
+        
+        List<List<String>> ret = new ArrayList<>();
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        helper(0, s, ret, new ArrayList<>(), dp);
+        return ret;
+    }
+    
+    public void helper(int start, String s, List<List<String>> ret, List<String> currList, boolean[][] dp) {
+        // base case
+        if(s.length() - start == 0) {
+            ret.add(new ArrayList<>(currList));
+            return;
+        }
+        
+        for(int i = start; i < s.length(); i++) {
+            // String temp = s.substring(start,i+1);
+            // if(!isPalindrone(temp)) continue;
+            if(s.charAt(start) == s.charAt(i) && (i-start <=2 || dp[start+1][i-1])) {
+                dp[start][i] = true;
+                currList.add(s.substring(start, i+1));
+                helper(i+1, s, ret, currList, dp);
+                currList.remove(currList.size() - 1);
+            }
+        }
+        
+        return;
+    }
+    
+    public boolean isPalindrone(String s) {
+        int p1 = 0, p2 = s.length() - 1;
+        while(p1 < p2) {
+            if(s.charAt(p1) == s.charAt(p2)) {
+                p1++;
+                p2--;
+                continue;
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
