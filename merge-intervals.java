8b2677c7abd94e64112a438103a4fa6128a63bad
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2020-11-26 23:03:44 -0600

    Update merge-intervals.java

--- merge-intervals.java
+++ merge-intervals.java
@@ -1,38 +1,22 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        
-        int[][] ret = new int[intervals.length][2];
-        int size = 0;
-        int[] curr = null;
-        
-        // sort list!
-        Arrays.sort(intervals, (int[] i1, int[] i2) ->  i1[0] - i2[0]);
-        
-        // use a stack
-        for(int i = 0; i < intervals.length; i++) {
-            if(curr == null) {
-                curr = new int[2];
-                curr[0] = intervals[i][0];
-                curr[1] = intervals[i][1];
-            } else {
-                if(intervals[i][0] <= curr[1]){
-                    if(intervals[i][1] > curr[1])
-                        curr[1] = intervals[i][1];
-                }
-                    
-                else {
-                    ret[size++] = curr;
-                    curr = new int[2];
-                    curr[0] = intervals[i][0];
-                    curr[1] = intervals[i][1];
-                }
-            }
-        }
-        
-        if(curr != null) ret[size++] = curr;
-        
-        //return intervals;
-        return Arrays.copyOfRange(ret, 0, size);
-        
-    }
+    public int[][] merge(int[][] intervals) {
+        
+        int[][] ret = new int[intervals.length][2];
+        int size = 0;
+        
+        // sort list!
+        Arrays.sort(intervals, (int[] i1, int[] i2) ->  i1[0] - i2[0]);
+        
+        // use a stack
+        for(int i = 0; i < intervals.length; i++) {
+            // if size == 0 or there is no overlap, put inside list
+            if(size == 0 || ret[size - 1][1] < intervals[i][0])
+                ret[size++] = intervals[i];
+            else // there's overlap
+                ret[size - 1][1] = Math.max(ret[size-1][1], intervals[i][1]);
+        }
+                
+        return Arrays.copyOfRange(ret, 0, size);
+        
+    }
 }
