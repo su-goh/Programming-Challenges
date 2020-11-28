470c040e8ca4000fa0902fb36322d4690710f34e
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2020-11-28 17:59:07 -0600

    Update implement-strstr.java

--- implement-strstr.java
+++ implement-strstr.java
@@ -1,73 +1,89 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        
-        if (needle.length() == 0) return 0;
-        if (haystack.length() == 0) return -1;
-        
-        //KMP APPROACH
-        int[] pattern = KMP(needle);
-        int j = 0;
-        
-        for(int i = 0; i < haystack.length(); i++) {
-            
-            if (haystack.charAt(i) != needle.charAt(j)) {
-                if(j > 0) {
-                    j = pattern[j-1];
-                    i--;
-                }
-                continue;
-            }
-​
-            // haystack[i] == needle[j]
-            if (j == needle.length() - 1) return i - j;
-            else j++;
-        }
-        
-        return -1;
-    }
-    
-    // KMP algorithm O(n) speed and size
-    public int[] KMP(String needle) {
-        int[] pattern = new int[needle.length()];
-        int j = 0;
-        
-        for(int i = 1; i < needle.length(); i++) {
-            // pattern[i] = 0, but already initialized as 0
-            if(needle.charAt(i) != needle.charAt(j)) {
-                if(j > 0) {
-                    j = pattern[j-1];
-                    i--;
-                }
-                continue;
-            }
-            
-            // if needle[i] == needle[j], pattern[i] = j + 1;
-            pattern[i] = ++j;
-        }
-        
-        return pattern;
-    }
-        
-        
-        //NAIIVE APPROACH 
-        
-//         if(needle.length() == 0) return 0;
-//         if(haystack.length() == 0) return -1; //|| haystack.length() < needle.length()
-//         int skip = 0;
-​
-        
-//         for(int i = 0; i < haystack.length(); i++) {
-//             // not a match
-//             // if (haystack.charAt(i) != needle.charAt(0)) continue;
-//             // might be a match
-            
-//             // if (needle.length() > i + haystack.length()) break;
-            
-//             for(int j = 0; j < needle.length(); j++) {
-                
-//                 // surely not a match
-//                 //if (haystack.charAt(i+j) != needle.charAt(j));
-                
-//                 // out bound of i checking
-//                  if (i+j == haystack.length()) return -1;
-                
+    public int strStr(String haystack, String needle) {
+        
+        if (needle.length() == 0) return 0;
+        if (haystack.length() == 0) return -1;
+        
+        //KMP APPROACH
+        int[] pattern = KMP(needle);
+        int j = 0;
+        
+        for(int i = 0; i < haystack.length(); i++) {
+            
+            if (haystack.charAt(i) != needle.charAt(j)) {
+                if(j > 0) {
+                    j = pattern[j-1];
+                    i--;
+                }
+                continue;
+            }
+
+            // haystack[i] == needle[j]
+            if (j == needle.length() - 1) return i - j;
+            else j++;
+        }
+        
+        return -1;
+    }
+    
+    // KMP algorithm O(n) speed and size
+    public int[] KMP(String needle) {
+        int[] pattern = new int[needle.length()];
+        int j = 0;
+        
+        for(int i = 1; i < needle.length(); i++) {
+            // pattern[i] = 0, but already initialized as 0
+            if(needle.charAt(i) != needle.charAt(j)) {
+                if(j > 0) {
+                    j = pattern[j-1];
+                    i--;
+                }
+                continue;
+            }
+            
+            // if needle[i] == needle[j], pattern[i] = j + 1;
+            pattern[i] = ++j;
+        }
+        
+        return pattern;
+    }
+        
+        
+        //NAIIVE APPROACH 
+        
+//         if(needle.length() == 0) return 0;
+//         if(haystack.length() == 0) return -1; //|| haystack.length() < needle.length()
+//         int skip = 0;
+
+        
+//         for(int i = 0; i < haystack.length(); i++) {
+//             // not a match
+//             // if (haystack.charAt(i) != needle.charAt(0)) continue;
+//             // might be a match
+            
+//             // if (needle.length() > i + haystack.length()) break;
+            
+//             for(int j = 0; j < needle.length(); j++) {
+                
+//                 // surely not a match
+//                 //if (haystack.charAt(i+j) != needle.charAt(j));
+                
+//                 // out bound of i checking
+//                  if (i+j == haystack.length()) return -1;
+                
+//                 // following is not a match // search from haystack again
+//                 if (haystack.charAt(i+j) != needle.charAt(j)) {
+//                     //i += skip;
+//                     break;
+//                 }
+                
+//                 // is a match
+//                 if (j == needle.length() - 1) return i;
+
+//                 // following might be a match
+//                 skip++;
+//             }
+//         }
+        
+//         return -1;
+}
