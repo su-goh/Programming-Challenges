c235aea17f494b88806f5848b867a20b77d3d534
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2020-11-30 10:59:32 -0600

    working commit - Created using LeetHub

---
+++ coin-change-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        Arrays.sort(coins);
+        int[] dp = new int[amount + 1];
+                
+        dp[0] = 1;
+        
+        for(int i = 0; i < coins.length; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(j - coins[i] >= 0) {
+                    dp[j] += dp[j-coins[i]];
+                }
+            }
+        }
+        
+        for(int i = 0; i <= amount; i++) System.out.println(dp[i]);
+        
+        return dp[amount];
+    }
+}
