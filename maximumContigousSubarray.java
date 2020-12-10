84e90121f10d205311de67f1ce4976864373cb45
su-goh <49136637+su-goh@users.noreply.github.com>
2020-12-10 11:50:25 -0600

    Added DP solution that's more right

--- maximumContigousSubarray.java
+++ maximumContigousSubarray.java
@@ -21,18 +21,35 @@ class maxSub {
             }
         }
 
-        if(currSum >= max) {
-            max = currSum;
-            maxStart = currStart;
-            maxEnd = a.length-1;
-        }
-
         return Arrays.copyOfRange(a, maxStart, maxEnd+1);
     }
 
+    public static int[] slidingWindow2(int[] a) {
+        int[] s = new int[a.length+1];
+        
+        for(int i = 0; i < a.length; i++) {
+            s[i+1] = Math.max(a[i], s[i] + a[i]);
+        }
+
+        int max = Integer.MIN_VALUE, maxJ = 0, maxI = 0;
+        for(int i = 1; i < s.length; i++) {
+            if(s[i] > max) {
+                max = s[i];
+                maxJ = i;
+            }
+        }        
+
+        maxI = maxJ;
+        while(max != 0) {
+            max -= s[maxI--];
+        }
+
+        return Arrays.copyOfRange(a, maxI, maxJ);
+    }
+
     public static void main(String[] args) {
-        int[] a = new int[]{0, -11, 9, 3, -2, 10, -5, 1, 2};
-        int[] b = slidingWindow(a);
+        int[] a = new int[]{-1,-2,-3,-4,-5};
+        int[] b = slidingWindow2(a);
         for(int i = 0; i < b.length; i++) System.out.print(b[i] + " ");
     }
 }
