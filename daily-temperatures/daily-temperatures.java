71ca503fea553896750e1edf814a724524326273
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-01-04 22:11:34 -0600

    Update daily-temperatures.java

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -1,16 +1,14 @@
 class Solution {
-    public int[] dailyTemperatures(int[] T) {
-        int[] D = new int[T.length];
-        
-        for(int i = 0; i < T.length; i++) {
-            for(int j = i+1; j < T.length; j++) {
-                if(T[j] > T[i]) {
-                    D[i] = j-i;
-                    break;
-                }
-            }
-        }
-        
-        return D;
-    }
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> st = new Stack<>();
+        int[] ret = new int[T.length];
+        
+        for(int i = T.length - 1; i >= 0; --i) {
+            while(!st.isEmpty() && T[st.peek()] <= T[i]) st.pop();
+            ret[i] = st.isEmpty() ? 0 : st.peek() - i;
+            st.push(i);
+        }
+
+        return ret;
+    }
 }
