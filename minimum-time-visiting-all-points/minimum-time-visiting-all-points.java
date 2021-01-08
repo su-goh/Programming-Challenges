89e9567178f18c9d3e9de3d248bc36eeca01b92e
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-01-08 11:39:56 -0600

    Time: 0 ms (100%), Space: 38.4 MB (81.24%)

--- minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -1,38 +1,18 @@
 class Solution {
-    public int minTimeToVisitAllPoints(int[][] points) {
-        int time = 0;
-        int[] curr = points[0];
-        
-        for(int i = 1; i < points.length; i++) {
-            int x = points[i][0];
-            int y = points[i][1];
-            
-            if(curr[0] == x && curr[1] == y) {
-                continue;
-            } else if (curr[0] == x && curr[1] != y) {
-                time += Math.abs(curr[1] - y);
-                curr = points[i];
-            } else if (curr[0] != x && curr[1] == y) {
-                time += Math.abs(curr[0] - x);
-                curr = points[i];
-            } else if (curr[0] != x && curr[1] != y) {
-                int dirX = Math.abs(x - curr[0]);
-                int dirY = Math.abs(y - curr[1]);
-                if(dirX == dirY) {
-                    time += dirX;
-                    curr = points[i];
-                } else if (dirX > dirY) {
-                    time += dirX;
-                    curr = points[i];
-                } else if (dirX < dirY) {
-                    time += dirY;
-                    curr = points[i];
-                }
-                
-            }
-        }
-        
-        return time;
-        
-    }
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        int[] curr = points[0];
+        
+        for(int i = 1; i < points.length; i++) {
+            int diffX = Math.abs(points[i][0] - curr[0]);
+            int diffY = Math.abs(points[i][1] - curr[1]);
+            
+            time += Math.max(diffX, diffY);
+            
+            curr = points[i];
+        }
+        
+        return time;
+        
+    }
 }
