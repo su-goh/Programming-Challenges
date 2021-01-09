66e0ba5c77966e696cee0030c4bcefc2858dbcfe
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-01-09 13:13:26 -0600

    Time: 1 ms (94.23%), Space: 38.3 MB (96.49%)

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,11 +1,18 @@
 class Solution {
-    public int findNumbers(int[] nums) {
-        int num = 0;
-        
-        for(int i = 0; i < nums.length; i++) {
-            if((nums[i] + "").length()  % 2 == 0) num++;
-        }
-        
-        return num;
-    }
+    public int findNumbers(int[] nums) {
+        int num = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+            int counter = 0;
+            while(nums[i] > 0) {
+                counter++;
+                nums[i] /= 10;
+            }
+            
+            if(counter % 2 == 0) num++;
+        }
+        
+        return num;
+    }
 }
