9b413909650818f46f113bd5111f7c48634ba8bc
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-01-22 16:53:03 -0600

    Time: 9 ms (5.93%), Space: 37.1 MB (14.45%) - LeetHub

---
+++ maximum-69-number/maximum-69-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String s = num + "";
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '6') {
+                s = s.substring(0, i) + "9" + s.substring(i+1, s.length());
+                num = Integer.parseInt(s);
+                break;
+            }
+        }
+                
+        return num;
+    }
+}
