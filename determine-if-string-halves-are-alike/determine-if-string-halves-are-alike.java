2c92aca077b8a4e5b9306ae9e8afeebf8b2ce5f7
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-01-22 16:44:25 -0600

    Update determine-if-string-halves-are-alike.java

--- determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,19 +1,18 @@
 class Solution {
-    public boolean halvesAreAlike(String s) {
-        int num = 0;
-        
-        for(int i = 0; i < s.length()/2; i++) {
-            Character curr = Character.toLowerCase(s.charAt(i));
-            if(!(curr.equals('a') || curr.equals('e') || curr.equals('i') || curr.equals('o') || curr.equals('u'))) continue;
-            num++;
-        }
-                
-        for(int i = s.length()/2; i < s.length(); i++){
-            Character curr = Character.toLowerCase(s.charAt(i));
-            if(!(curr.equals('a') || curr.equals('e') || curr.equals('i') || curr.equals('o') || curr.equals('u'))) continue;
-            num--;
-        }
-                
-        return (num == 0) ? true : false;
-    }
+    public boolean halvesAreAlike(String s) {
+        int num = 0;
+        String vowels = "AEIOUaeiou";
+        
+        for(int i = 0; i < s.length()/2; i++) {
+            if(vowels.indexOf(s.charAt(i)) == -1) continue;
+            num++;
+        }
+                
+        for(int i = s.length()/2; i < s.length(); i++){
+            if(vowels.indexOf(s.charAt(i)) == -1) continue;
+            num--;
+        }
+                
+        return (num == 0) ? true : false;
+    }
 }
