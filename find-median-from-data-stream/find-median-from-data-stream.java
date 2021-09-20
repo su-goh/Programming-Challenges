a5a132c3e9428766d71494c2b5540b510702b2a2
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-09-19 21:36:40 -0500

    Time: 372 ms (5.07%), Space: 129.2 MB (6.95%) - LeetHub

--- find-median-from-data-stream/find-median-from-data-stream.java
+++ find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,35 +1,38 @@
 class MedianFinder {
-    
-    PriorityQueue<Integer> h1;
-    PriorityQueue<Integer> h2;
+
+    PriorityQueue<Integer> leftMax;
+    PriorityQueue<Integer> rightMin;
     int size;
     
     /** initialize your data structure here. */
     public MedianFinder() {
-        // max-pq of first half, min-pq of second half
-        h1 = new PriorityQueue<>(Collections.reverseOrder());
-        h2 = new PriorityQueue<>();
+        leftMax = new PriorityQueue<>(Collections.reverseOrder());
+        rightMin = new PriorityQueue<>();
         size = 0;
     }
     
     public void addNum(int num) {
+        if(size % 2 == 0) leftMax.add(num);
+        else rightMin.add(num);
         size++;
         
-        if(size % 2 == 0) {
-            h2.add(num);
-            h1.add(h2.poll());
-        } else {
-            h1.add(num);
-            h2.add(h1.poll());
+        while (size > 1 && leftMax.peek() > rightMin.peek()) {
+            // System.out.println(size + " " + (size % 2 != 0 && leftMax.size() > Math.ceil(size/2.0)));
+            int leftTemp = leftMax.poll();
+            int rightTemp = rightMin.poll();
+            rightMin.add(leftTemp);
+            leftMax.add(rightTemp);
         }
-        System.out.println(h1.peek() + " " + h2.peek() + " " + size);
-
     }
     
     public double findMedian() {
-        // System.out.println(h1.peek() + " " + h2.peek() + " " + size);
-        if (size % 2 == 0) return (h1.peek() + h2.peek())/2.0;
-        else return h2.peek();
+        if(size % 2 == 0) {
+            System.out.println("both " + leftMax.peek() + " " + rightMin.peek());
+            return (leftMax.peek() + rightMin.peek())/2.0;
+        } else {
+            System.out.println("not both");
+            return (double) leftMax.peek();
+        }
     }
 }
 
