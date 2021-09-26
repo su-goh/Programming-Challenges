eb976d70cb69e78427de711f3f6e34b5797f678f
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-09-25 19:09:48 -0500

    Time: 21 ms (91.80%), Space: 44.3 MB (40.92%) - LeetHub

---
+++ random-pick-with-weight/random-pick-with-weight.java
@@ -0,0 +1,33 @@
+class Solution {
+    int[] accumulatedW;
+    Random rdm;
+    
+    public Solution(int[] w) {
+        accumulatedW = new int[w.length];
+        rdm = new Random();
+        for(int i = 0; i < w.length; i++) {
+            accumulatedW[i] = (i == 0) ? w[i] : w[i] + accumulatedW[i - 1];
+        }
+    }
+    
+    public int pickIndex() {
+        int target = 1 + rdm.nextInt(accumulatedW[accumulatedW.length - 1]);
+        
+        int l = 0, r = accumulatedW.length - 1;
+        while(l < r) {
+            int m = l + (r - l)/2;
+            
+            if(accumulatedW[m] < target) l = m + 1;
+            else if(accumulatedW[m] > target) r = m;
+            else return m;
+        }
+        
+        return l;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
