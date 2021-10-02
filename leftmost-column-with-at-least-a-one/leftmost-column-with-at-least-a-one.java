f8cd7dbb126ece17bae60e2091826dd7eded19a7
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-10-01 22:37:01 -0500

    Time: 1 ms (26.54%), Space: 44.7 MB (17.53%) - LeetHub

---
+++ leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.java
@@ -0,0 +1,41 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface BinaryMatrix {
+ *     public int get(int row, int col) {}
+ *     public List<Integer> dimensions {}
+ * };
+ */
+
+class Solution {
+    int minCol;
+    BinaryMatrix binaryMatrix;
+    
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        minCol = Integer.MAX_VALUE;
+        this.binaryMatrix = binaryMatrix;
+        
+        List<Integer> dim = binaryMatrix.dimensions();
+        int row = dim.get(0);
+        int col = dim.get(1);
+        
+        for(int i = 0; i < row; i++) {
+            binarySearch(i, 0, col - 1);
+        }
+        
+        return minCol == Integer.MAX_VALUE ? -1 : minCol;
+    }
+    
+    private void binarySearch(int i, int start, int end) {
+        while(start < end) {
+            int mid = start + (end - start)/2;
+            if(binaryMatrix.get(i, mid) == 1) {
+                end = mid;
+                minCol = Math.min(minCol, mid);
+            } else start = mid + 1;
+        }
+        
+        int mid = start + (end - start)/2;
+        if(binaryMatrix.get(i, mid) == 1) minCol = Math.min(minCol, mid);
+    }
+}
