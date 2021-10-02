10999ba59b3d7af24c462634e9f1daa29d1c8aef
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-10-01 21:36:49 -0500

    Time: 2 ms (45.92%), Space: 38.1 MB (57.14%) - LeetHub

--- minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,21 +1,18 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        int[] dp = new int[days[days.length - 1] + 1];
+        Queue<int[]> weekPass = new LinkedList<>(), monthPass = new LinkedList<>();
+        int cost = 0;
         
-        Set<Integer> travelDays = new HashSet<>();
-        for(int day: days) travelDays.add(day);
-        
-        for(int i = 1; i < dp.length; i++) {
-            if(!travelDays.contains(i)) dp[i] = (i - 1) > 0 ? dp[i - 1] : 0;
-            else {
-                int prevCost = (i - 1) > 0 ? dp[i - 1] : 0;
-                int sevenCost = (i - 7) > 0 ? dp[i - 7] : 0;
-                int thirtyCost = (i - 30) > 0 ? dp[i - 30] : 0;
-                
-                dp[i] = Math.min(prevCost + costs[0], Math.min(sevenCost + costs[1], thirtyCost + costs[2]));
-            }
+        for(int day : days) {
+            while(weekPass.size() > 0 && weekPass.peek()[0] < day) weekPass.poll();
+            while(monthPass.size() > 0 && monthPass.peek()[0] < day) monthPass.poll();
+            
+            weekPass.add(new int[]{day + 6, cost + costs[1]});
+            monthPass.add(new int[]{day + 29, cost + costs[2]});
+
+            cost = Math.min(cost + costs[0], Math.min(weekPass.peek()[1], monthPass.peek()[1]));
         }
         
-        return dp[dp.length - 1];
+        return cost;
     }
 }
