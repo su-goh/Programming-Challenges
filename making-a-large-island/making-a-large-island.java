c9be32ff188035732704f657b4e2167563520e5b
Sunanda Xi Yue Goh <49136637+su-goh@users.noreply.github.com>
2021-10-03 18:24:49 -0500

    Time: 1161 ms (5.06%), Space: 82.7 MB (41.65%) - LeetHub

--- making-a-large-island/making-a-large-island.java
+++ making-a-large-island/making-a-large-island.java
@@ -1,53 +1,49 @@
 class Solution {
-    int[][] map;
     int[][] grid;
     HashMap<Integer, Integer> sizes;
     
     public int largestIsland(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        this.grid = grid;
         int max = 0;
-        map = new int[m][n];
+        this.grid = grid;
         sizes = new HashMap<>();
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1 && map[i][j] == 0)
+                if(grid[i][j] == 1)
                     countSize(new int[]{i, j});
             }
         }
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                // System.out.print(map[i][j] + " ");
                 if(grid[i][j] == 0) {
                     int currMax = 1;
                     HashSet<Integer> visitedId = new HashSet<>();
                     
-                    if(i - 1 >= 0 && grid[i - 1][j] == 1 && !visitedId.contains(map[i - 1][j])) {
-                        visitedId.add(map[i - 1][j]);
-                        currMax += sizes.get(map[i - 1][j]);
+                    if(i - 1 >= 0 && grid[i - 1][j] > 0 && !visitedId.contains(grid[i - 1][j])) {
+                        visitedId.add(grid[i - 1][j]);
+                        currMax += sizes.get(grid[i - 1][j]);
                     }
-                    if(i + 1 < m && grid[i + 1][j] == 1  && !visitedId.contains(map[i + 1][j])) {
-                        visitedId.add(map[i + 1][j]);
-                        currMax += sizes.get(map[i + 1][j]);
+                    if(i + 1 < m && grid[i + 1][j] > 0  && !visitedId.contains(grid[i + 1][j])) {
+                        visitedId.add(grid[i + 1][j]);
+                        currMax += sizes.get(grid[i + 1][j]);
                     }
-                    if(j - 1 >= 0 && grid[i][j - 1] == 1 && !visitedId.contains(map[i][j - 1])) {
-                        visitedId.add(map[i][j - 1]);
-                        currMax += sizes.get(map[i][j - 1]);
+                    if(j - 1 >= 0 && grid[i][j - 1] > 0 && !visitedId.contains(grid[i][j - 1])) {
+                        visitedId.add(grid[i][j - 1]);
+                        currMax += sizes.get(grid[i][j - 1]);
                     }
-                    if(j + 1 < n && grid[i][j + 1] == 1  && !visitedId.contains(map[i][j + 1])) {
-                        visitedId.add(map[i][j + 1]);
-                        currMax += sizes.get(map[i][j + 1]);
+                    if(j + 1 < n && grid[i][j + 1] > 0  && !visitedId.contains(grid[i][j + 1])) {
+                        visitedId.add(grid[i][j + 1]);
+                        currMax += sizes.get(grid[i][j + 1]);
                     }
                     
                     max = Math.max(max, currMax);
                 }
             }
-            // System.out.println();
         }
         
-        return max != 0 ? max : (grid[0][0] == 0 ? 0 : sizes.get(map[0][0]));        
+        return max != 0 ? max : (grid[0][0] == 0 ? 0 : sizes.get(grid[0][0]));        
     }
     
     private void countSize(int[] loc) {
@@ -56,13 +52,13 @@ class Solution {
         Queue<int[]> q = new LinkedList<>();
         q.add(loc);
         visited[loc[0]][loc[1]] = true;
-        int id = sizes.size();
+        int id = sizes.size() + 2;
         
         while(!q.isEmpty()) {
             int[] currLoc = q.poll();
             int i = currLoc[0], j = currLoc[1];
             size++;
-            map[i][j] = id;
+            grid[i][j] = id;
             
             if(i - 1 >= 0 && grid[i - 1][j] == 1 && !visited[i - 1][j]) {
                 visited[i - 1][j] = true;
@@ -83,31 +79,5 @@ class Solution {
         }
         
         sizes.put(id, size);
-        
-//         visited = new boolean[grid.length][grid[0].length];
-//         q.add(loc);
-//         while(!q.isEmpty()) {
-//             int[] currLoc = q.poll();
-//             int i = currLoc[0], j = currLoc[1];
-//             map[i][j] = size;
-            
-//             if(i - 1 >= 0 && grid[i - 1][j] == 1 && !visited[i - 1][j]) {
-//                 visited[i - 1][j] = true;
-//                 q.add(new int[]{i - 1, j});
-//             }
-//             if(i + 1 < visited.length && grid[i + 1][j] == 1 && !visited[i + 1][j]) {
-//                 visited[i + 1][j] = true;
-//                 q.add(new int[]{i + 1, j});
-//             }
-//             if(j - 1 >= 0 && grid[i][j - 1] == 1 && !visited[i][j - 1]) {
-//                 visited[i][j - 1] = true;
-//                 q.add(new int[]{i, j - 1});
-//             }
-//             if(j + 1 < visited[0].length && grid[i][j + 1] == 1 && !visited[i][j + 1]) {
-//                 visited[i][j + 1] = true;
-//                 q.add(new int[]{i, j + 1});
-//             }
-//         }
-        
     }
 }
